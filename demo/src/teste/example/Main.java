63f4b1515cdddaf021ec4ff4a2f14527729c9d39
devdinei <devdinei@gmail.com>
2024-09-05 22:20:16 -0300

    mais exercicios

---
+++ demo/src/teste/example/Main.java
@@ -0,0 +1,16 @@
+package teste.example;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Mensagem de boas-vindas
+        System.out.println("Hello, World!");
+        
+        // Chamada de um método adicional (opcional)
+        greetUser("Usuário");
+    }
+
+    // Método adicional para demonstrar funcionalidade
+    public static void greetUser(String name) {
+        System.out.println("Bem-vindo, " + name + "!");
+    }
+}
